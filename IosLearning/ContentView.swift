cd9d431b99797d49369c12d3ce0a3dd88e314a2e
hemanthkumar <hemanth@rigi.club>
2022-06-11 00:20:29 +0530

    Initial Commit

---
+++ IosLearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IosLearning
+//
+//  Created by Hemanth Kumar on 11/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IosLearning/IosLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosLearningApp.swift
+//  IosLearning
+//
+//  Created by Hemanth Kumar on 11/06/22.
+//
+
+import SwiftUI
+
+@main
+struct IosLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
